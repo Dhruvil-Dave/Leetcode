33aa547f54596deb2d36e36887d0bee533806d8b
Dhruvil Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2022-12-04 12:45:07 +0530

    Time: 8 ms (31.43%), Space: 54.6 MB (23.26%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -2,10 +2,9 @@ class Solution {
     public int[] findBall(int[][] grid) {
         final int m = grid.length;
         final int n = grid[0].length;
-        // dp[i] := status of i-th column
-        // -1 := empty, 0 := b0, 1 := b1, ...
+        
         int[] dp = new int[n];
-        // ans[i] := i-th ball's final position
+        
         int[] ans = new int[n];
         Arrays.fill(ans, -1);
 
@@ -16,10 +15,10 @@ class Solution {
           int[] newDp = new int[n];
           Arrays.fill(newDp, -1);
           for (int j = 0; j < n; ++j) {
-            // Out of bound
+            
             if (j + grid[i][j] < 0 || j + grid[i][j] == n)
               continue;
-            // Stuck
+            
             if (grid[i][j] == 1 && grid[i][j + 1] == -1 || grid[i][j] == -1 && grid[i][j - 1] == 1)
               continue;
             newDp[j + grid[i][j]] = dp[j];
