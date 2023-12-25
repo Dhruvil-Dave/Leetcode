4d8d5a29b4a3890c206d6630475762732ad8dfd3
Dhruvil Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2023-12-25 09:25:21 +0530

    Time: 1 ms (84.19%), Space: 41.4 MB (15.73%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,18 +1,26 @@
 class Solution {
-    public int numDecodings(String s) {
-        if(s.length() == 0 || s.charAt(0) == '0') return 0;
-        if(s.length() == 1) return 1;
-        
-        int count1 = 1, count2 = 1;
-        for(int i = 1; i < s.length(); ++i){
-            int d = s.charAt(i) - '0';
-            int dd = (s.charAt(i-1) - '0')*10 + d;
-            int count = 0;
-            if(d > 0) count += count2;
-            if(dd >= 10 && dd <= 26) count += count1;
-            count1 = count2;  
-            count2 = count;  
-        }
-        return count2;
+  public int numDecodings(String s) {
+    final int n = s.length();
+    // dp[i] := the number of ways to decode s[i..n)
+    int[] dp = new int[n + 1];
+    dp[n] = 1; // ""
+    dp[n - 1] = isValid(s.charAt(n - 1)) ? 1 : 0;
+
+    for (int i = n - 2; i >= 0; --i) {
+      if (isValid(s.charAt(i)))
+        dp[i] += dp[i + 1];
+      if (isValid(s.charAt(i), s.charAt(i + 1)))
+        dp[i] += dp[i + 2];
     }
+
+    return dp[0];
+  }
+
+  private boolean isValid(char c) {
+    return c != '0';
+  }
+
+  private boolean isValid(char c1, char c2) {
+    return c1 == '1' || c1 == '2' && c2 < '7';
+  }
 }
