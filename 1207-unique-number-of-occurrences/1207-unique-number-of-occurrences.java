66be412adae371d8ce5a82d609b0a9198bb2d842
Dhruvil Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2024-01-17 12:39:29 +0530

    Time: 2 ms (98.10%), Space: 42.2 MB (5.39%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -3,14 +3,13 @@ class Solution {
         Map<Integer, Integer> count = new HashMap<>();
         Set<Integer> occurrences = new HashSet<>();
 
-        for (int a : arr)
-          count.put(a, count.getOrDefault(a, 0) + 1);
+        for (final int a : arr)
+          count.merge(a, 1, Integer::sum);
 
-        for (int value : count.values())
+        for (final int value : count.values())
           if (!occurrences.add(value))
             return false;
 
         return true;
-
     }
 }
