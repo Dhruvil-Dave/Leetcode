89a0fd0a3dc14f227e7fd41c66e95ade957bb2c8
Dhruvil Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2024-01-20 21:02:12 +0530

    Time: 18 ms (93.51%), Space: 49.5 MB (40.16%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,47 +1,32 @@
 class Solution {
-    public int sumSubarrayMins(int[] arr) {
-//         int sum=0;
-//         int n = arr.length;
-//         for(int i=0;i<n;i++){
-//             int min=arr[i];
-//             for(int k=i;k<n;k++){
-//                 if(min>arr[k]){
-//                     min=arr[k];
-                    
-//                     //sum+=min;
-//                 }
-//                 sum+=min;
-//                 // System.out.println(sum);
-//                 // System.out.println(min+" this is min");
-                
-//             }
-//         }
-//         return sum;
-        int kMod = 1_000_000_007;
-        int n = arr.length;
-        long ans = 0;
-        int[] prev = new int[n];
-        int[] next = new int[n];
-        Deque<Integer> stack = new ArrayDeque<>();
+  public int sumSubarrayMins(int[] arr) {
+    final int kMod = 1_000_000_007;
+    final int n = arr.length;
+    long ans = 0;
+    // prevMin[i] := index k s.t. arr[k] is the previous minimum in arr[:i]
+    int[] prevMin = new int[n];
+    // nextMin[i] := index k s.t. arr[k] is the next minimum in arr[i + 1:]
+    int[] nextMin = new int[n];
+    Deque<Integer> stack = new ArrayDeque<>();
 
-        Arrays.fill(prev, -1);
-        Arrays.fill(next, n);
+    Arrays.fill(prevMin, -1);
+    Arrays.fill(nextMin, n);
 
-        for (int i = 0; i < arr.length; ++i) {
-          while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
-            int index = stack.pop();
-            next[index] = i;
-          }
-          if (!stack.isEmpty())
-            prev[i] = stack.peek();
-          stack.push(i);
-        }
-
-        for (int i = 0; i < arr.length; ++i) {
-          ans += (long) arr[i] * (i - prev[i]) * (next[i] - i);
-          ans %= kMod;
-        }
-
-        return (int) ans;
+    for (int i = 0; i < arr.length; ++i) {
+      while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+        final int index = stack.pop();
+        nextMin[index] = i;
+      }
+      if (!stack.isEmpty())
+        prevMin[i] = stack.peek();
+      stack.push(i);
     }
+
+    for (int i = 0; i < arr.length; ++i) {
+      ans += (long) arr[i] * (i - prevMin[i]) * (nextMin[i] - i);
+      ans %= kMod;
+    }
+
+    return (int) ans;
+  }
 }
